daf0a1bc7f4a5b54cf8c0b1e1d3e933d9cbbbd67
Nicola Ferraro <nibbio84@gmail.com>
2015-09-12 02:05:40 +0200

    Zookeeper alpha

---
+++ core/setup/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=3.4.6
+
+tar -xzf zookeeper-$VERSION.tar.gz
+mv zookeeper-$VERSION /usr/lib
+ln -s /usr/lib/zookeeper-$VERSION /usr/lib/zookeeper
+
+mv zoo.cfg /usr/lib/zookeeper/conf
+mv dockmobStart.sh /usr/lib/zookeeper/bin
+
+
